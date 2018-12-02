a02c509e77fbd8d12198e0aad2f08646ac443f16
Ahmed Abdelfattah <ahmed.fattah98@eng-st.cu.edu.eg>
2018-12-02 15:38:13 +0200

    Study Activiy

---
+++ app/src/main/java/com/abdelfattah/study/Student.kt
@@ -0,0 +1,12 @@
+package com.abdelfattah.study
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Student : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student)
+    }
+}
